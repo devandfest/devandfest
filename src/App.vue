64b14e39fdefd4d5340254d907621776f372e376
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2018-10-11 14:39:12 +0200

    Fix Travis
    Signed-off-by: Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App, We are working in the new website"/>
   </div>
 </template>
 
